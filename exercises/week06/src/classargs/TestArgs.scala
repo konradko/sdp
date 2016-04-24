16bcc6bce8cc99044d154ccc8bcd8db5edbcd6f3
konradko <konrad.korzel@gmail.com>
2016-04-24 22:08:00 +0100

    Add flexible family

--- exercises/week06/src/classargs/TestArgs.scala
+++ exercises/week06/src/classargs/TestArgs.scala
@@ -7,9 +7,18 @@ object TestArgs extends App {
   family1.familySize() is 4
   val family2 = new Family("Dad", "Mom", "Harry")
   family2.familySize() is 3
+  val family3 = new FlexibleFamily("Mum", "Dad", "Sally", "Dick")
+  family3.familySize() is 4
+  val family4 = new FlexibleFamily("Dad", "Mom", "Harry")
+  family4.familySize() is 3
 }
 
 class Family(members:String*) {
-
   def familySize() : Int = members.length
 }
+
+
+class FlexibleFamily(mother: String, father: String, children: String*) {
+  def familySize() : Int = 2 + children.length
+}
+
