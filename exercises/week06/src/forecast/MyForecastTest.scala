c171476e4805f7ad433b659e7a2f80daa1c99c45
konradko <konrad.korzel@gmail.com>
2016-04-24 21:44:53 +0100

    Add answer 03, classargs

--- exercises/week06/src/forecast/MyForecastTest.scala
+++ exercises/week06/src/forecast/MyForecastTest.scala
@@ -8,6 +8,8 @@ object MyForecastTest extends App {
   forecast(20) is "Mostly Sunny"
   forecast(0) is "Cloudy"
   forecast(15) is "Unknown"
+
+  // Answer 01
   def forecast(temp: Int): String = temp match {
     case 100 => "Sunny"
     case 80 => "Mostly Sunny"
@@ -16,4 +18,12 @@ object MyForecastTest extends App {
     case 0 => "Cloudy"
     case _ => "Unknown"
   }
+
+  // Answer 02
+  val vec = Vector(100, 80, 50, 20, 0, 15)
+  for(temp <- vec){
+    println(forecast(temp))
+  }
 }
+
+
